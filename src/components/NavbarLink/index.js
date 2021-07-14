aa47a6542654f1ece7e4e1a7d9ef698f38ab8c24
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-07-14 12:50:29 -0300

    Cria transição para sibebar menu oculto

--- src/components/NavbarLink/index.js
+++ src/components/NavbarLink/index.js
@@ -1,13 +1,15 @@
-import React, { useState, useCallback } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import style from './style.module.css'
 
-const NavbarLink = ({icon, text, path, pathname}) => {
+const NavbarLink = ({icon, text, path, pathname, retracted}) => {
     let active = path === pathname
     active = pathname === '/' && path === '/horas' ? active = true : active
 
     return (
-        <Link to={path} className={`${style.container} ${active ? style.containerActive : ''}`}>
+        <Link to={path} className={`${style.container}
+                                    ${active ? style.containerActive : ''}
+                                    ${retracted ? style.retracted : ''}`}>
             <img className={style.icon} src={icon}/>
             <span className={style.text} >{text}</span>
         </Link>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useCallback } from 'react'
 import {ReactComponent as Retract} from '../../assets/retract.svg'
 import Hours from '../../assets/hours.svg'
 import OrganizationsIcon from '../../assets/organizations.svg'
@@ -10,31 +10,47 @@ import { Logo } from '../Logo'
 
 const Sidebar = ({active}) => {
     const [pathname, setPathname] = useState(window.location.pathname)
-
-
+    const [retracted, setRetracted] = useState(false)
+    const [currentOpacity, setOpacity] = useState('1')
     const pages = [
         {name: 'horas', text: 'Minhas Horas', icon: Hours, path: '/horas'},
         {name: 'atividades', text: 'Minhas Atividades', icon: activitiesIcon, path: '/atividades'},
         {name: 'orgs', text: 'Organizações', icon: OrganizationsIcon, path: '/orgs'},
         {name: 'duvidas', text: 'Minhas Horas', icon: QuestionsIcon, path: '/duvidas'},
     ]
+    
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
     const handlePathname = useCallback(() => {
         setPathname(window.location.pathname)
-    }, [pathname, setPathname])
+    }, [setPathname])
+
+    const handleRetract = useCallback(async () => {
+        setRetracted(!retracted)
+        setOpacity('0')
+        await sleep(400)
+        setOpacity('1')
+    }, [retracted, setRetracted])
+
+
 
     return (
-        <div className={style.container}>
+        <div className={`${style.container} ${retracted ? style.containerRetracted : style.fullContainer}`}>
             <div className={style.topHeader} onClick={handlePathname}>
-                <Retract className={style.retract}/>
-                <Logo />
+                <Retract
+                    onClick={handleRetract}
+                    className={`${style.retract}`}
+                    style={{opacity: currentOpacity}}/>
+                <div className={style.logo}><Logo/></div>
             </div>
-            
-            <ul>
+
+            <ul className={style.links}>
                 {pages.map((page) => {
                     return (
                         <li id={page.name} onClick={handlePathname}>
-                            <NavbarLink text={page.text} icon={page.icon} path={page.path} pathname={pathname}/>
+                            <NavbarLink text={page.text} icon={page.icon} path={page.path} pathname={pathname} retracted={retracted}/>
                         </li>
                     )
                 })}
