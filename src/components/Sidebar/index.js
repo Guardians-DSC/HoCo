372173a5ed567c4f6559760e2e895b4644c28244
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-07-14 15:03:26 -0300

    Adiciona posição fixed na navbar e refatora componentes para se adequar a essa mudança

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react'
-import {ReactComponent as Retract} from '../../assets/retract.svg'
+import { ReactComponent as Retract } from '../../assets/retract.svg'
 import OrganizationsIcon from '../../assets/organizations.svg'
 import activitiesIcon from '../../assets/activities.svg'
 import QuestionsIcon from '../../assets/questions.svg'
@@ -10,17 +10,17 @@ import NavbarLink from '../NavbarLink'
 import { Profile } from '../Profile'
 import { Logo } from '../Logo'
 
-const Sidebar = ({active}) => {
+const Sidebar = ({ active }) => {
     const [pathname, setPathname] = useState(window.location.pathname)
     const [retracted, setRetracted] = useState(false)
     const [currentOpacity, setOpacity] = useState('1')
     const pages = [
-        {name: 'horas', text: 'Minhas Horas', icon: Hours, path: '/horas'},
-        {name: 'atividades', text: 'Minhas Atividades', icon: activitiesIcon, path: '/atividades'},
-        {name: 'orgs', text: 'Organizações', icon: OrganizationsIcon, path: '/orgs'},
-        {name: 'duvidas', text: 'Minhas Horas', icon: QuestionsIcon, path: '/duvidas'},
+        { name: 'horas', text: 'Minhas Horas', icon: Hours, path: '/horas' },
+        { name: 'atividades', text: 'Minhas Atividades', icon: activitiesIcon, path: '/atividades' },
+        { name: 'orgs', text: 'Organizações', icon: OrganizationsIcon, path: '/orgs' },
+        { name: 'duvidas', text: 'Minhas Horas', icon: QuestionsIcon, path: '/duvidas' },
     ]
-    
+
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
@@ -37,28 +37,33 @@ const Sidebar = ({active}) => {
     }, [retracted, setRetracted])
 
     return (
-        <div className={`${style.container} ${retracted ? style.containerRetracted : style.fullContainer}`}>
-            <div className={style.topHeader} onClick={handlePathname}>
-                <Retract
-                    onClick={handleRetract}
-                    className={`${style.retract}`}
-                    style={{opacity: currentOpacity}}/>
-                <div className={style.logo}><Logo/></div>
-            </div>
+        <div className={`${style.outContainer} ${retracted ? style.outContainerRetracted : ''}`}>
+            <div className={`${style.container} ${retracted ? style.containerRetracted : style.fullContainer}`}>
+                <div className={style.topHeader} onClick={handlePathname}>
+                    <Retract
+                        onClick={handleRetract}
+                        className={`${style.retract}`}
+                        style={{ opacity: currentOpacity }} />
+                    <div className={style.logo}>
+                        <Logo />
+                    </div>
+                </div>
 
-            <ul className={style.links}>
-                {pages.map((page) => {
-                    return (
-                        <li id={page.name} onClick={handlePathname}>
-                            <NavbarLink text={page.text} icon={page.icon} path={page.path} pathname={pathname} retracted={retracted}/>
-                        </li>
-                    )
-                })}
-            </ul>
+                <ul className={style.links}>
+                    {pages.map((page) => {
+                        return (
+                            <li id={page.name} onClick={handlePathname}>
+                                <NavbarLink text={page.text} icon={page.icon} path={page.path} pathname={pathname} retracted={retracted} />
+                            </li>
+                        )
+                    })}
+                </ul>
 
-            <div className={style.profile} onClick={handlePathname}>
-                <Profile photo={Photo} name='Rodrigo Eloy' retracted={retracted}/>
+                <div className={style.profile} onClick={handlePathname}>
+                    <Profile photo={Photo} name='Rodrigo Eloy' retracted={retracted} />
+                </div>
             </div>
+            <div></div>
         </div>
     )
 }
