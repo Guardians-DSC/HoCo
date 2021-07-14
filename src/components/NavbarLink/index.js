f758f316fb1bd20b14635ab267338248eabcbf59
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-07-14 11:00:21 -0300

    Refatora forma de ativar para inclusive path '/'

--- src/components/NavbarLink/index.js
+++ src/components/NavbarLink/index.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom'
 import style from './style.module.css'
 
 const NavbarLink = ({icon, text, path, pathname}) => {
+    let active = path === pathname
+    active = pathname === '/' && path === '/horas' ? active = true : active
 
     return (
-        <Link to={path} className={`${style.container} ${path === pathname ? style.containerActive : ''}`}>
+        <Link to={path} className={`${style.container} ${active ? style.containerActive : ''}`}>
             <img className={style.icon} src={icon}/>
             <span className={style.text} >{text}</span>
         </Link>
